0b9c29a244f8c0d9fd024a309e8900860a7cc249
hawnzug <hawnzug@gmail.com>
2016-11-02 19:13:39 +0800

    add config file

--- src/main.rs
+++ src/main.rs
@@ -14,25 +14,45 @@ use std::fs::File;
 use std::path::Path;
 use std::io::BufReader;
 use std::io::BufRead;
+use std::collections::HashMap;
 
 header! { (XAccessToken, "X-Access-Token") => [String] }
 header! { (XClientID, "X-Client-ID") => [String] }
 
-fn get_headers() -> Headers {
+type Config = HashMap<String, String>;
+
+fn get_config() -> Result<Config, String> {
     let path = Path::new(".wunderist");
     let f = File::open(&path).unwrap();
     let file = BufReader::new(&f);
-    let mut lines = file.lines();
-    let mut headers = Headers::new();
-    headers.set(XClientID(lines.next().unwrap().unwrap()));
-    headers.set(XAccessToken(lines.next().unwrap().unwrap()));
-    headers
+
+    let mut config = HashMap::new();
+    for (i, line) in file.lines().enumerate() {
+        let l = line.unwrap();
+        let v: Vec<&str> = l.split(':').map(str::trim).collect();
+        if v.len() != 2 {
+            return Err(format!("config error: more than one colons at line {}", i + 1));
+        }
+        config.insert(v[0].to_string(), v[1].to_string());
+    }
+    Ok(config)
 }
 
-fn get_user() {
+fn get_headers(config: &Config) -> Result<Headers, String> {
+    let mut headers = Headers::new();
+    let token = try!(config.get("X-Access-Token")
+                           .ok_or("No X-Access-Token in config!".to_string()));
+    let id = try!(config.get("X-Client-ID")
+                        .ok_or("No X-Client-ID in config!".to_string()));
+    headers.set(XClientID(id.to_string()));
+    headers.set(XAccessToken(token.to_string()));
+    Ok(headers)
+}
+
+fn get_user(config: &Config) {
     let client = Client::new();
     let url = Url::parse("http://a.wunderlist.com/api/v1/user").unwrap();
-    let request = client.get(url).headers(get_headers());
+    let request = client.get(url).headers(get_headers(config).unwrap());
     let mut res = request.send().unwrap();
     let mut buf = String::with_capacity(65535);
     match res.read_to_string(&mut buf) {
@@ -49,10 +69,10 @@ fn get_user() {
     }
 }
 
-fn get_lists() {
+fn get_lists(config: &Config) {
     let client = Client::new();
     let url = Url::parse("http://a.wunderlist.com/api/v1/lists").unwrap();
-    let request = client.get(url).headers(get_headers());
+    let request = client.get(url).headers(get_headers(config).unwrap());
     let mut res = request.send().unwrap();
     let mut buf = String::with_capacity(65535);
     match res.read_to_string(&mut buf) {
@@ -72,11 +92,13 @@ fn get_lists() {
     }
 }
 
-fn get_inbox_id() -> u64 {
-    return 195185253; // TODO: add this to config file
+fn get_inbox_id(config: &Config) -> u64 {
+    if let Some(ref id) = config.get("inbox-id") {
+        return id.parse().unwrap();
+    }
     let client = Client::new();
     let url = Url::parse("http://a.wunderlist.com/api/v1/lists").unwrap();
-    let request = client.get(url).headers(get_headers());
+    let request = client.get(url).headers(get_headers(config).unwrap());
     let mut res = request.send().unwrap();
     let mut buf = String::with_capacity(65535);
     match res.read_to_string(&mut buf) {
@@ -98,12 +120,12 @@ fn get_inbox_id() -> u64 {
     return 0;
 }
 
-fn get_inbox() {
-    let id = get_inbox_id().to_string();
+fn get_inbox(config: &Config) {
+    let id = get_inbox_id(config).to_string();
     let client = Client::new();
     let mut url = Url::parse("http://a.wunderlist.com/api/v1/tasks").unwrap();
     url.query_pairs_mut().clear().append_pair("list_id", &id);
-    let request = client.get(url).headers(get_headers());
+    let request = client.get(url).headers(get_headers(config).unwrap());
     let mut res = request.send().unwrap();
     let mut buf = String::with_capacity(65535);
     match res.read_to_string(&mut buf) {
@@ -120,11 +142,11 @@ fn get_inbox() {
     }
 }
 
-fn add_task_inbox(name: &str) {
+fn add_task_inbox(name: &str, config: &Config) {
     let client = Client::new();
     let url = Url::parse("http://a.wunderlist.com/api/v1/tasks").unwrap();
     let data = format!("{{\"list_id\": 195185253, \"title\": \"{}\"}}", name);
-    let mut headers = get_headers();
+    let mut headers = get_headers(config).unwrap();
     headers.set(ContentType(Mime(TopLevel::Application, SubLevel::Json, vec![])));
     let request = client.post(url).headers(headers).body(&data);
     let mut res = request.send().unwrap();
@@ -149,20 +171,28 @@ fn main() {
                                                .index(1)))
                       .get_matches();
 
+    let config = match get_config() {
+        Ok(config) => config,
+        Err(err) => {
+            println!("{}", err);
+            return;
+        }
+    };
+
     if matches.subcommand_matches("user").is_some() {
-        get_user();
+        get_user(&config);
     }
 
     if matches.subcommand_matches("lists").is_some() {
-        get_lists();
+        get_lists(&config);
     }
 
     if matches.subcommand_matches("inbox").is_some() {
-        get_inbox();
+        get_inbox(&config);
     }
 
     if let Some(m) = matches.subcommand_matches("add") {
         let name = m.value_of("Task Name").unwrap();
-        add_task_inbox(name);
+        add_task_inbox(name, &config);
     }
 }
